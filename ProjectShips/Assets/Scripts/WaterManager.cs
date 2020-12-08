44ebe1255181e5e2313c5ea1fbe2f09b5e10d02d
scanax2 <scanax2@gmail.com>
2020-12-08 10:07:09 +0100

    Added visualization of a water sin wave & effect on the objects

---
+++ ProjectShips/Assets/Scripts/WaterManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class WaterManager : MonoBehaviour
+{
+    private MeshFilter meshFilter;
+
+    //public float noiseWalk = 1f;
+    //public float noiseStrength = 1f;
+
+    //public float speed = 2f;
+    //public float scale = 1f;
+
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    private void Update()
+    {
+        Vector3[] vertices = meshFilter.mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = WaveManager.instance.GetWaveHeight(transform.position.x + vertices[i].x);
+            //vertices[i].y += Mathf.Sin(Time.time * speed + vertices[i].x + vertices[i].y + vertices[i].z) * scale;
+            //vertices[i].y += Mathf.PerlinNoise(vertices[i].x + noiseWalk, vertices[i].x + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
+
+        }
+
+        meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.RecalculateNormals();
+    }
+}
